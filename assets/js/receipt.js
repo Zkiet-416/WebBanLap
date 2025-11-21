2e64071037dafda2dd7be03905bec123a119c54d
quocan529 <quocan529@gmail.com>
2025-11-21 10:40:49 +0700

    Đổi bp1.png sang BP1.PNG

--- assets/js/receipt.js
+++ assets/js/receipt.js
@@ -82,7 +82,7 @@ function LoadReceipt() {
       id: "PN001",
       date: "2025-10-28",
       products: [
-        { name: "Bàn phím Akko MonsGeek M1W HE-SP V3 Dark Night", price: "2835000", qty: "10", imagePath: "../assets/images/bp1.png" }, 
+        { name: "Bàn phím Akko MonsGeek M1W HE-SP V3 Dark Night", price: "2835000", qty: "10", imagePath: "../assets/images/BP1.png" }, 
         { name: "Chuột Razer Cobra - Zenless Zone One Edition", price: "990000", qty: "15", imagePath: "../assets/images/Mouse1.jpg" },
       ],
     },
@@ -269,3 +269,4 @@ function LoadReceipt() {
   window.saveInvoice = saveInvoice;
   window.searchInvoice = searchInvoice;
 }
+
